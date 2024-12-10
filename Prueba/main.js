66ec4513f0a9bfd647b764fc57358b3c3d197396
Enrique Hernandez <enrinandez51@gmail.com>
2024-12-10 01:35:06 -0600

    Prueba 2 de git con visual kike

--- Prueba/main.js
+++ Prueba/main.js
@@ -1,8 +1,12 @@
 console.log('PRUEBA DE CONEXION A GIT HUB XD')
 
-function saludar(nombre){
-    console.log('Hola ${nombre}');
-
+function saludar(nombres){
+    console.log('Hola ${nombres}');
 }
+
+/*1. git init
+2. git add . (para agregar todos los archivos nuevos/modificados)
+3. git commit -m "[Mensaje identificador]"
+4. git push origin [nombre de la rama] */
 
 saludar("kike");
